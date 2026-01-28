d64819226324c236cd51df8d0e42385b92a1c599
Kili2222 <supe.laukkanen@gmail.com>
2026-01-28 20:57:42 +0200

    tehtava 2.2

---
+++ tehtava_2.2.py
@@ -0,0 +1,7 @@
+import math
+def laske_ympyran_pintaala():
+    sade=float(input("Anna ympyrän säde: "))
+    pintaala=math.pi*sade**2
+    print(f"pinta-ala on= {pintaala:.2f}")
+laske_ympyran_pintaala()
+
